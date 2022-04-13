1db0610b424f173bb50ad0de7d15920a98a970b5
Ivann LARUELLE <ivann+git@laruelle.me>
2022-04-13 22:10:13 +0200

    add maths to semesters

--- pyutttils/Semester.py
+++ pyutttils/Semester.py
@@ -21,14 +21,69 @@ class Semester:
     def __str__(self):
         return self.full_name
 
+    def __sub__(self, other):
+        if isinstance(other, Semester):
+            semesters = 2 * (self.full_year - other.full_year)
+            if semesters >= 0 and self.short_season != other.short_season:
+                if self.short_season == "P" and other.short_season == "A":
+                    semesters -= 1
+                else:
+                    semesters += 1
+            elif semesters < 0:
+                semesters = (other - self) * -1
+            return semesters
+        elif isinstance(other, int):
+            if other < 0:
+                return self.__add__(other * -1)
+            semester = self
+            for i in range(other):
+                semester = semester.get_previous()
+            return semester
+        return NotImplemented
+
+    def __eq__(self, other):
+        if isinstance(other, Semester):
+            return self.full_name == other.full_name
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Semester):
+            return self - other > 0
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Semester):
+            return self - other < 0
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Semester):
+            return not self.__gt__(other)
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Semester):
+            return not self.__lt__(other)
+        return NotImplemented
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            if other < 0:
+                return self.__sub__(other * -1)
+            semester = self
+            for i in range(other):
+                semester = semester.get_next()
+            return semester
+        return NotImplemented
+
     def get_next(self):
         if self.short_season == "A":
-            return Semester("P"+str(self.full_year+1))
+            return Semester("P" + str(self.full_year + 1))
         else:
             return Semester("A" + str(self.full_year))
 
     def get_previous(self):
         if self.short_season == "A":
-            return Semester("P"+str(self.full_year))
+            return Semester("P" + str(self.full_year))
         else:
             return Semester("A" + str(self.full_year - 1))
